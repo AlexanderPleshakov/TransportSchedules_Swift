8591e465a96f5d24ebcc3fec740c7536f81380a7
AlexanderPleshakov <sanya.pleshakov.95@mail.ru>
2024-09-26 19:21:09 +0300

    Basic project configuration

---
+++ TransportSchedules_Swift/App/SceneDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  SceneDelegate.swift
+//  TransportSchedules_Swift
+//
+//  Created by Александр Плешаков on 26.09.2024.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: windowScene)
+        window?.windowScene = windowScene
+        window?.makeKeyAndVisible()
+        window?.rootViewController = SearchViewController()
+    }
+}
+
