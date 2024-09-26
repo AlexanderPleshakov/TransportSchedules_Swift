284c59b8b40272d019ed3cefdb8fbd3ce5a66f92
AlexanderPleshakov <sanya.pleshakov.95@mail.ru>
2024-09-26 19:04:23 +0300

    Initial Commit

---
+++ TransportSchedules_Swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TransportSchedules_Swift
+//
+//  Created by Александр Плешаков on 26.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
