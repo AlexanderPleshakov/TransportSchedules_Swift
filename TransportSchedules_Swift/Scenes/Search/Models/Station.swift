2688992910f7907485702c0d3a4384c040426b5e
AlexanderPleshakov <sanya.pleshakov.95@mail.ru>
2024-09-28 15:21:43 +0300

    Add station model

---
+++ TransportSchedules_Swift/Scenes/Search/Models/Station.swift
@@ -0,0 +1,9 @@
+struct Station {
+    let code: String
+    let stationType: String
+    let transportType: String
+    let title: String
+    let countryName: String
+    let regionName: String
+    let settlementName: String
+}
